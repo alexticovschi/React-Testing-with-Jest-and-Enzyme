b65308afb498c20917a3e0750e13ff5836b669e2
Alex Ticovschi <alexmticovschi@gmail.com>
2020-08-20 19:24:22 +0100

    using Enzyme in a test

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,8 +1,12 @@
 import React from "react";
-import Enzyme from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders learn react link", () => {});
+test("renders without crushing", () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+  expect(wrapper).toBeFalsy();
+});
