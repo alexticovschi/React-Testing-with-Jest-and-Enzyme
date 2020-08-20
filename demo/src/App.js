722118dae00942cb1678b467d09a6679fb132d0b
Alex Ticovschi <alexmticovschi@gmail.com>
2020-08-20 19:06:38 +0100

    Enzyme setup

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Learn React Testing</h1>      
+      <h1>Learn React Testing!!</h1>
     </div>
   );
 }
