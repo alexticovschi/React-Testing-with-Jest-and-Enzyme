e6f0c530e4852fb101cc1c0d1df0f233f9bb57ff
Alex Ticovschi <alexmticovschi@gmail.com>
2020-08-21 13:19:59 +0100

    tested component rendering

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import Enzyme, {
-  shallow
-} from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import App from './App';
+import App from "./App";
 
 Enzyme.configure({
-  adapter: new EnzymeAdapter()
+  adapter: new EnzymeAdapter(),
 });
 
-test('renders without error', () => {
-
+test("renders without error", () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
 
-test('renders increment button', () => {
+test("renders increment button", () => {});
 
-});
-
-test('renders counter display', () => {
-
-});
+test("renders counter display", () => {});
 
 // test the state
-test('counter starts at 0', () => {
-
-});
+test("counter starts at 0", () => {});
 
 // test functionality of the button
-test('clicking button increments counter display', () => {
-
-});
+test("clicking button increments counter display", () => {});
